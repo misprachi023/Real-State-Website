2b4dcb7b43d7ade5143e2b14a1a53c6889666c7e
misprachi023 <prachimishra23123@gmail.com>
2024-07-02 21:28:42 +0530

    add hashedpassword code

--- api/index.js
+++ api/index.js
@@ -1,6 +1,8 @@
 import express from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
+import userRouter from "./routes/user.route.js";
+import authRouter from "./routes/auth.route.js";
 dotenv.config();
 
 mongoose.connect(process.env.MONGO).then(() => {
@@ -9,10 +11,22 @@ mongoose.connect(process.env.MONGO).then(() => {
     console.log(err);
 })
 const app = express();
+app.use(express.json());
 
-// prachimishra23123
-// SC2XB17tyAM4LuzP
 
 app.listen(3000, () => {
     console.log("Server started on port 3000");
 })
+
+app.use("/api/user", userRouter);
+app.use("/api/auth", authRouter);
+
+app.use((err, req, res, next) => {
+    const statusCode = err.statusCode || 500;
+    const message = err.message || "Internal Server Error";
+    return res.status(statusCode).json({
+      success: false,
+      statusCode,
+      message,
+    });
+  });
