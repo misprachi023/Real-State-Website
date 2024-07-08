949b951e585b22d29f4442561d142b2e2aa906a7
misprachi023 <prachimishra23123@gmail.com>
2024-07-08 21:09:53 +0530

    add profile page

--- client/src/components/OAuth.jsx
+++ client/src/components/OAuth.jsx
@@ -1,29 +1,51 @@
 import { GoogleAuthProvider, getAuth, signInWithPopup } from "firebase/auth";
 import { app } from "../firebase.js";
+import { useDispatch } from "react-redux";
+import {signInSuccess} from "../redux/user/userSlice.js";
+import { useNavigate } from "react-router-dom";
+
 
 const OAuth = () => {
-  const handleGoogleClick = async () => {
-    try {
-      const provider = new GoogleAuthProvider();
-      const auth = getAuth(app);
 
-      const result = await signInWithPopup(auth, provider);
+    const dispatch = useDispatch();
+    const navigate = useNavigate();
 
-      console.log(result);
-    } catch (error) {
-      console.log("Could not sign in with Google", error.message);
-    }
-  };
+    const handleGoogleClick = async () => {
+        try {
+            const provider = new GoogleAuthProvider();
+            const auth = getAuth(app);
+
+            const result = await signInWithPopup(auth, provider);
+            
+            const res = await fetch("/api/auth/google", {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json",
+                },
+                body: JSON.stringify({
+                    name: result.user.displayName,
+                    email: result.user.email,
+                    photo: result.user.photoURL,
+                }),
+            });
+
+            const data = await res.json();
+
+            dispatch(signInSuccess(data));
+            navigate("/");         
+
+            
+        } catch (error) {
+            console.log('Could not sign in with Google', error);
+        }
+    };
+
 
   return (
-    <button
-      onClick={handleGoogleClick}
-      type="button"
-      className="bg-red-700 text-white p-3 rounded-lg uppercase hover:opacity-95"
-    >
+    <button onClick={handleGoogleClick} type="button" className="bg-red-700 text-white p-3 rounded-lg uppercase hover:opacity-95">
       Continue with Google
     </button>
-  );
+  )
 };
 
 export default OAuth;
--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -2,7 +2,34 @@ import React from 'react'
 
 const Profile = () => {
   return (
-    <div>Profile</div>
+  
+    <div className="p-3 max-w-lg mx-auto">
+      <h1 className="text-3xl font-semibold text-center my-7">Profile</h1>
+      <form className="flex flex-col gap-4">
+        <input
+          type="text"
+          placeholder="username"
+          id="username"
+          className="border p-3 rounded-lg"
+        />
+
+        <input
+          type="text"
+          placeholder="email"
+          id="email"
+          className="border p-3 rounded-lg"
+        />
+
+        <input
+          type="password"
+          placeholder="password"
+          id="password"
+          className="border p-3 rounded-lg"
+        />
+        <button className="bg-slate-700 text-white p-3 rounded-lg uppercase hover:opacity-95 disabled:opacity-80">Submit</button>
+      </form>
+      
+    </div>
   )
 }
 
